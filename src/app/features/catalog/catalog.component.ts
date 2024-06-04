4b129066e49cbfaf9924372bf8a8d0e03d2ae727
https://github.com/anisharuxia <anisharuxia04@gmail.com>
2024-06-04 21:38:31 +0400

    update
    search input and start responsive

--- src/app/features/catalog/catalog.component.ts
+++ src/app/features/catalog/catalog.component.ts
@@ -1,11 +1,12 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { AppComponent } from '../../app.component';
 import { ApiService } from '../../shared/api.service';
 import { product } from './productmodel';
 import { CommonModule } from '@angular/common';
+import { FormsModule, NgControl, NgModel, NgModelGroup } from '@angular/forms';
 
 
 
@@ -13,13 +14,17 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [FooterComponent, RouterOutlet, AppComponent, CommonModule,RouterLink],
+  imports: [FooterComponent, RouterOutlet, AppComponent, CommonModule,RouterLink,FormsModule],
   templateUrl: './catalog.component.html',
-  styleUrl: './catalog.component.css'
+  styleUrl: './catalog.component.css',
+  
 })
 export class CatalogComponent implements OnInit  {
-  data!:any|product[]
-  constructor(private api:ApiService){}
+  data!:any|product[];
+  searchtext:any;
+  constructor(private api:ApiService){};
+
+
   
   ngOnInit(): void {
    this.displayproduct();
